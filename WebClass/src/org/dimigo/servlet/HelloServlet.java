4d256b6c8983b9ee7061f61b62941aaa4467aa15
조아진 <@DESKTOP-1B91DC2>
2017-09-22 14:48:55 +0900

    commit

---
+++ WebClass/src/org/dimigo/servlet/HelloServlet.java
@@ -0,0 +1,91 @@
+package org.dimigo.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class HelloServlet
+ */
+@WebServlet(description = "첫번째 서블릿", urlPatterns = { "/Hello" })
+public class HelloServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+    /**
+     * Default constructor. 
+     */
+    public HelloServlet() {
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		//response.getWriter().append("Served at: ").append(request.getContextPath());
+		
+		//요철데이터 처리
+		request.setCharacterEncoding("utf-8");
+		String id = request.getParameter("id");
+		String name = request.getParameter("name");
+		System.out.printf("id : $s, name : %s\n", id ,name);
+		
+		//출력데이터 Content Type 설정
+		response.setContentType("text/html;charset=utf-8");
+		PrintWriter out = response.getWriter();
+		
+		out.println("<!DOCTYPE html>");
+		out.println("<html>");
+		out.println("<head>");
+		out.println("<title>Servlet Test</title>");
+		out.println("</head>");
+		out.println("<body>");
+		out.println("<h1>Hello</h1>");
+		out.println("<h2>id:" + id + ", name:" + name +"</h2>");
+		out.println("</body>");
+		out.println("</html>");
+		
+		out.close();
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+	/* (non-Javadoc)
+	 * @see javax.servlet.GenericServlet#init(javax.servlet.ServletConfig)
+	 */
+	@Override
+	public void init(ServletConfig config) throws ServletException {
+		System.out.println("init() 호출");
+	}
+	
+	
+	 /* (non-Javadoc)
+	 * @see javax.servlet.http.HttpServlet#service(javax.servlet.http.HttpServletRequest, javax.servlet.http.HttpServletResponse)
+	 */
+	@Override
+	protected void service(HttpServletRequest arg0, HttpServletResponse arg1) throws ServletException, IOException {
+		System.out.println("service() 호출");
+		super.service(arg0, arg1);
+	}
+	
+	/* (non-Javadoc)
+	 * @see javax.servlet.GenericServlet#destroy()
+	 */
+	@Override
+	public void destroy() {
+		System.out.println("destroy() 호출");
+	}
+}
